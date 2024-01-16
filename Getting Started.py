1215455ced3ef5ccecc2cda0a10c3e618da40858
Ahmed Kiser <ahmed.kiser@northsouth.edu>
2024-01-16 12:33:39 +0600

    image read

---
+++ Getting Started.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 12:27:11 2024
+
+@author: User
+"""
+
+import cv2 #openCV use as cv2 in python
+img1 = cv2.imread("Images/checkerboard_18x18.png")
+
+print(img1)
