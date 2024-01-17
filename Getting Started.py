acdc411a6a0de67a841c772d64b0981102ba91f7
Ahmed Kiser <ahmed.kiser@northsouth.edu>
2024-01-17 19:28:52 +0600

    file read and write added

--- Getting Started.py
+++ Getting Started.py
@@ -6,6 +6,19 @@ Created on Tue Jan 16 12:27:11 2024
 """
 
 import cv2 #openCV use as cv2 in python
-img1 = cv2.imread("Images/checkerboard_18x18.png")
-
+img1 = cv2.imread("Images/New_Zealand_Lake.jpg",1)
+img1 = cv2.resize(img1, (700,700))
 print(img1)
+cv2.imshow("my image", img1)
+
+
+img2 = cv2.imread("Images/New_Zealand_Lake.jpg",-1)
+img2 = cv2.resize(img2, (700,700))
+print(img2)
+cv2.imshow("grayscale image", img2)
+
+
+
+cv2.waitKey()
+cv2.destroyAllWindows()
+
